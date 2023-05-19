95a19b092ee457503f4538a597c4a94ea0ad1810
Sebastian009w <ia3266595@gmail.com>
2023-05-19 10:33:00 -0500

    dependecies and imports map

--- main.ts
+++ main.ts
@@ -6,4 +6,5 @@ app.use(oakCors()); // Enable CORS for All Routes
 app.use(router.routes());
 app.use(router.allowedMethods());
 
+console.log("example: http://localhost:8000");
 await app.listen({ port: 8000 });
